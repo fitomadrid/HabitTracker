8332adcac5150690ac49f8fc3726e152c96caf77
fitomadrid <russel379363@gmail.com>
2024-08-25 06:46:57 -0400

    clean code

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
